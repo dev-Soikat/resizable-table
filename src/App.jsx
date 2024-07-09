7528ac55071073dcc0a91f6eddb11215f0b65716
soikatRoccSolution <soikat.chakrbarty@roccsolution.com>
2024-07-09 10:01:49 +0530

    mockdata update + dark mode setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-//
+import { useState } from "react";
+import { MdOutlineLightMode, MdDarkMode } from 'react-icons/md';
 
 const App = () => {
+  const [dark, setDark] = useState(false);
+
+  const toggleDarkMode = () => setDark(!dark);
+
   return (
-    <p className="text-red-500 font-qsand text-5xl">Soikat's Stantech Assignment</p>
+    <div className={dark && 'dark'}>
+      <div className="dark:bg-black h-[100vh]">
+        <div className='flex justify-between items-center p-4'>
+          <p className="text-red-500 dark:text-yellow-500 font-qsand text-3xl">Soikat's Stantech Assignment</p>
+
+          <div className='border border-black dark:border-white rounded-full p-1 cursor-pointer' onClick={toggleDarkMode}>
+            {dark ? <MdOutlineLightMode className="text-white" /> : <MdDarkMode />}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
